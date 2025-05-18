a1ca1afe4b8d6b6185c056c88192d1d232582ff1
Favouriteless <62647254+Favouriteless@users.noreply.github.com>
2025-05-18 01:57:41 +0100

    Fixed template bug

--- common/src/main/java/net/favouriteless/modopedia/api/Lookup.java
+++ common/src/main/java/net/favouriteless/modopedia/api/Lookup.java
@@ -1,16 +1,39 @@
 package net.favouriteless.modopedia.api;
 
+import net.favouriteless.modopedia.api.books.TemplateProcessor;
+import net.favouriteless.modopedia.api.books.page_components.PageComponent;
+import net.favouriteless.modopedia.book.variables.RemoteVariable;
+
 import java.util.Collection;
 
+
+/**
+ * Wraps a Map of {@link Variable}s for use in {@link PageComponent}s and {@link TemplateProcessor}s.
+ */
 public interface Lookup {
 
+    /**
+     * @return The value attached to the given key. If no value is found, throw an exception.
+     */
     Variable get(String key);
 
+    /**
+     * @return The value attached to the given key. If no value is found, return the default instead.
+     */
     Variable getOrDefault(String key, Object def);
 
+    /**
+     * @return True if this Lookup contains a value for key. ({@link RemoteVariable}s will defer up the hierarchy until
+     * they find a value)
+     */
     boolean has(String key);
 
+    /**
+     * @return Collection of all keys present in the lookup.
+     */
     Collection<String> keys();
+
+
 
     /**
      * Mutable version of {@link Lookup} used for template processors.
